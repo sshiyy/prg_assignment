103a52ed1b089fa8ed07636dd4e6033f155ff34f
sshiyy <148183319+sshiyy@users.noreply.github.com>
2024-01-11 14:02:03 +0800

    Update class.cs

--- class.cs
+++ class.cs
@@ -16,7 +16,47 @@ namespace prg_assignment
         {
             get { return name; }
             set { name = value; }
+        }
 
+        public int Memberid
+        {
+            get { return memberid; }
+            set { memberid = value; }
+        }
+
+        public DateTime Dob
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        public Order CurrentOrder
+        {
+            get { return currentOrder; }
+            set { currentOrder = value; }
+        }
+
+        public List<Order> OrderHistory
+        {
+            get { return orderHistory; }
+            set { orderHistory = value; }
+        }
+
+        public PointCard Rewards
+        {
+            get { return rewards; }
+            set { rewards = value; }
+        }
+
+        // constructors
+        public Customer() { }
+
+        public Customer(string name, int memberid, DateTime dob)
+        {
+            Name = name;
+            Memberid = memberid;
+            Dob = dob;
+        }
     }
 
     class Order

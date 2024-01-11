6633544e0bd017a5e981a5ee923fa02acbc63df7
sshiyy <148183319+sshiyy@users.noreply.github.com>
2024-01-11 13:38:59 +0800

    Create class.cs

---
+++ class.cs
@@ -0,0 +1,12 @@
+namespace prg_assignment
+{
+    // Customer 
+    class Customer
+    {
+        // private attributes
+        private string name;
+        private int memberid;
+        private DateTime dob;
+        private 
+    }
+}
